862cbe8dd71354b56da3c8fda6791c2cef30b332
unknown <ismaelsuring@gmail.com>
2025-05-01 19:11:18 -0700

    BUILD SUCCESFUL

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot_template-1";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "b4bee7851627a93349be232fc8ea5573a9f4fe5c";
+  public static final String GIT_DATE = "2025-05-01 21:35:37 EDT";
+  public static final String GIT_BRANCH = "advantageKit";
+  public static final String BUILD_DATE = "2025-05-01 22:09:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1746151786597L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
